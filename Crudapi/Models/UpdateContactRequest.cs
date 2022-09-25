0f58b1cc3b2b327d3ac578876eebfe152dc63eea
Athithyaraj <109848349+Athithyaraj@users.noreply.github.com>
2022-09-25 12:38:01 +0530

    Update UpdateContactRequest.cs
    DOJ update changes

--- Crudapi/Models/UpdateContactRequest.cs
+++ Crudapi/Models/UpdateContactRequest.cs
@@ -1,12 +1,12 @@
-﻿namespace Crudapi.Models
+namespace Crudapi.Models
 {
     public class UpdateContactRequest
     {
         public string token { get; set; }
-        public string empcode { get; set; }
+        public string? empcode { get; set; }
 
-        public string empname { get; set; }
+        public string? empname { get; set; }
 
-        public DateTime doj { get; set; }
+      //  public DateOnly doj { get; set; }
     }
 }
